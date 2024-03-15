19dc6b85e591c6ea088d039171c2f455f04957e6
Pauline Wairimu <wairimupauline759@gmail.com>
2024-03-14 18:26:12 -0700

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
 
-    Args:
-        a: first integer
-        b: second integer
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
