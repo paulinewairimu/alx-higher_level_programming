e0dfad78acef88f1d777f3e6899e87a2cd93e780
Pauline Wairimu <wairimupauline759@gmail.com>
2024-03-14 18:16:34 -0700

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Match bytecode provided by ALx."""
+    """Match bytecode provided by Alx"""
     from magic_calculation_102 import add, sub
 
     if a < b:
