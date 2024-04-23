1a1bca892f9abe6a3eafe443a3016e78818ede5a
Pauline Wairimu <wairimupauline759@gmail.com>
2024-04-23 04:04:31 -0700

    Write an empty class BaseGeometry.

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Defines a JSON file-writing function."""
-import json
+"""Defines an empty class BaseGeometry."""
 
 
-def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
